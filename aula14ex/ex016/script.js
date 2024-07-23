34a80b96acf45dbd12ab090107445a7862b91960
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-07-23 12:33:23 +0200

    Add empty string to clear the <p> after invalid input

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -6,17 +6,20 @@ function contar() {
 
     if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         alert("ERRO: Insira um número em cada campo!");
+        res.innerHTML = "";
     } else {
-        res.innerHTML = "Contando: "
+        res.innerHTML = "Contando: <br>"
         let ini = Number(inicio.value);
         let fi = Number(fim.value);
         let pas = Number(passo.value);
 
+        //contagem progressiva
         if(pas > 0 && fi > ini) {
             for(let c = ini; c <= fi; c += pas) {
                 res.innerHTML += `${c} \u{27A1} `;
             }
             res.innerHTML += ` \u{1F3C1}`
+        //contagem regressiva
         } else if(pas > 0 && fi < ini) {
             for(let c = ini; c >= fi; c -= pas) {
                 res.innerHTML += `${c} \u{27A1} `;
