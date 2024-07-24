1fc63bfb619bc5fccaa8c09924116875e6ecf37b
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-07-24 16:29:07 +0200

    Add aula16 folder with function exercises

---
+++ aula16/funcao02.js
@@ -0,0 +1,12 @@
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+let res = soma(2, 7);
+console.log(res);
+
+//é possível atribuir valor no parâmetro desse exemplo para evitar o NaN.
+
+/* function soma(n1=0, n2=0) {
+    return n1 + n2;
+} */
